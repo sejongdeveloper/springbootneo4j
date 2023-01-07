60ca2db2bf71fc0f58e587689fe7bd15c2aebe2a
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-07 23:26:03 +0900

    스프리 데이터 11부: Neo4j

--- src/main/java/me/whiteship/springbootneo4j/Neo4jRunner.java
+++ src/main/java/me/whiteship/springbootneo4j/Neo4jRunner.java
@@ -1,9 +1,8 @@
 package me.whiteship.springbootneo4j;
 
 import me.whiteship.springbootneo4j.account.Account;
+import me.whiteship.springbootneo4j.account.AccountRepository;
 import me.whiteship.springbootneo4j.account.Role;
-import org.neo4j.ogm.session.Session;
-import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -12,22 +11,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class Neo4jRunner implements ApplicationRunner {
     @Autowired
-    SessionFactory sessionFactory;
+    AccountRepository accountRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Account account = new Account();
-        account.setEmail("keesun@mail.com");
-        account.setUsername("keesun");
+        account.setEmail("aaaa@mail.com");
+        account.setUsername("aaaa");
 
         Role role = new Role();
-        role.setName("admin");
+        role.setName("user");
 
         account.getRoles().add(role);
 
-        Session session = sessionFactory.openSession();
-        session.save(account);
-        sessionFactory.close();
+        accountRepository.save(account);
 
         System.out.println("finished");
     }
